ee865426a6cc7f4083b0fb6c75102aba5a45b9f5
Martyna Zukowska <martynazukowska7@interia.pl>
2026-09-14 13:16:19 +0200

    Config SQL

--- config/settings.py
+++ config/settings.py
@@ -12,18 +12,22 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env( DJANGO_DEBUG=(bool, False), ) 
+environ.Env.read_env(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vs!#&ji1_rexbv*qr_@g(h!ay_dl9sw^6!pe77s0bcg_ws9df9'
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DJANGO_DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -74,8 +78,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DB_NAME"), 
+        "USER": env("DB_USER"), 
+        "PASSWORD": env("DB_PASSWORD"), 
+        "HOST": env("DB_HOST"), 
+        "PORT": env.int("DB_PORT", default=5432),
     }
 }
 
