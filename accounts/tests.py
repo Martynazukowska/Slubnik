733b24664fe3949bb40be8b55548d2c555a9a1df
Martyna Zukowska <martynazukowska7@interia.pl>
2026-09-14 14:23:01 +0200

    Add user profile model

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model 
+from django.test import TestCase 
+from .models import Profile 
 
-# Create your tests here.
+User = get_user_model() 
+
+class ProfileModelTests(TestCase): 
+    def test_profile_is_created_when_user_is_created(self): 
+        user = User.objects.create_user( 
+            username="testuser", 
+            password="Test12345!", 
+            ) 
+        self.assertTrue( Profile.objects.filter(user=user).exists() ) 
+    def test_user_can_access_profile(self): 
+        user = User.objects.create_user( 
+            username="testuser", 
+            password="Test12345!", 
+            ) 
+        self.assertEqual( user.profile.user, user, )
--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
 ]
+
+# Authentication 
+AUTH_USER_MODEL = "accounts.User"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
